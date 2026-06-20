501c405471feef836c56b46a219636c679355ffa
Maciej Brencz <maciej.brencz@gmail.com>
2026-06-20 16:19:12 +0100

    Add Umhvørvisstovan feed to pelicanconf.py

--- planet/pelicanconf.py
+++ planet/pelicanconf.py
@@ -58,6 +58,7 @@ PLANET_FEEDS = {
 	"Farerskie szorty": "https://farerskiekadry.pl/szorty/feed",
 	"r/FaroeIslands": "https://www.reddit.com/r/FaroeIslands.rss",
 	"Faroe Islands Daily": "https://faroeislandsdaily.com/feed/",
+	"Umhvørvisstovan": "https://www.us.fo/category/tidindi/feed/",
 }
 
 # PLANET_FEEDS = {"Farerskie kadry": "https://farerskiekadry.pl/feed", "Mastodon - @ForoysktDaily": "https://mastodon.social/@ForoysktDaily.rss", "Faroe Islands Daily": "https://faroeislandsdaily.com/feed/",} # debug
